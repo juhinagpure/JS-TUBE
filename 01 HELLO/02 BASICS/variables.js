da75f3f4e52d0d3ede4ee4e5e95c65ef4ec16e34
JuhiNagpure <juhinagpure143@gmail.com>
2022-07-14 11:02:08 +0530

    variables

--- 01 HELLO/02 BASICS/variables.js
+++ 01 HELLO/02 BASICS/variables.js
@@ -17,7 +17,10 @@ var loggedCount = 34;
 // console.log("loggedCount")
 
 var paymentMode;
-console.log(paymentMode);
+
 
 
 paymentMode = "Credit card";
+console.log(paymentMode);
+
+// var if = 23;
