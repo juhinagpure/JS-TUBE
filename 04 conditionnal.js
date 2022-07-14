013a6417778d5a11ec70368ac828503de7197496
JuhiNagpure <juhinagpure143@gmail.com>
2022-07-14 18:53:26 +0530

    conditions

---
+++ 04 conditionnal.js
@@ -0,0 +1,22 @@
+var temperature;
+
+//TODO:Go to google and get the data 
+
+
+temperature = 49;
+
+// var result = temperature < 20;
+// console.log(result);
+
+if (temperature < 20){
+    console.log("it's very cold outside")
+}
+
+if (temperature < 30){
+    console.log("its moderate outside")
+} else {
+    console.log("its really hot outside")
+}
+
+
+
