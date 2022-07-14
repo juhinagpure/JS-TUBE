e27a50c9e2ff46527141367cc2d4c4b29b8d12ea
JuhiNagpure <juhinagpure143@gmail.com>
2022-07-14 13:34:47 +0530

    variables

---
+++ 01 HELLO/02 BASICS/02 variables two.js
@@ -0,0 +1,23 @@
+const uid = "abc123";
+
+var fullName = "Juhi Nagpure";
+var email = "juhinagpure143@gmail.com";
+var password = "123456";
+var confirmPassword = "123456";
+var courseCount = 0;
+var isloggedInFromGoogle = false;
+
+
+// fullname = prompt("Enter yor name")
+
+
+console.log(uid);
+console.log("Full Name is:",fullName);
+console.log(email);
+
+console.log(`
+with unique ID: ${uid}
+User is : ${fullName}
+and his email is:${email}
+and uses the password:${password}
+`);
