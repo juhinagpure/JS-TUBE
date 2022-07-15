5f50bc425c14cc355fe8894ef26dea15d15c857d
JuhiNagpure <juhinagpure143@gmail.com>
2022-07-15 19:17:44 +0530

    functions

---
+++ 03 Intermediate/01 function.js
@@ -0,0 +1,16 @@
+function sayHello(name){
+   console.log("Hello there ,Juhi");
+   console.log(`Hello there,${name}.How are you?`);
+}
+
+// sayHello("Juhi");
+// sayHello("tukku");
+
+function namastey(){
+    return"Hello in India";
+}
+
+var greetings = namastey();
+
+console.log(greetings);
+console.log(namastey());
