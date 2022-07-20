66293675b1e6142cad9ecc73d0f2ddee3770bf6d
JuhiNagpure <juhinagpure143@gmail.com>
2022-07-20 21:20:40 +0530

    code hoisting

---
+++ 03 Intermediate/04 Hoisting.js
@@ -0,0 +1,29 @@
+tipper("80");
+
+function tipper(a){
+    var bill = parseInt(a);
+    console.log(bill + 5);
+};
+
+
+function bigTipper(a) {
+    var bill = parseInt(a);
+    console.log(bill + 15);
+};
+
+bigTipper("200");
+
+console.log(name);
+var name = "juhi"
+
+
+function sayName() {
+    var name = "MRS. J";
+    console.log(name);
+    
+}
+sayName();
+
+
+
+console.log(name);
