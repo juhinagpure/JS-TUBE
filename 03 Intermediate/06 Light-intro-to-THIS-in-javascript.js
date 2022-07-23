afec3f6932c1e65763c9e3d58e92e74735f3cdc7
JuhiNagpure <juhinagpure143@gmail.com>
2022-07-23 17:20:24 +0530

    THIS info

---
+++ 03 Intermediate/06 Light-intro-to-THIS-in-javascript.js
@@ -0,0 +1,14 @@
+// TOD0: part 1 for this keyword
+
+
+console.log(this);
+
+
+var game ="basketball";
+
+// function sayName(){
+//     var name = "juhi"
+//     console.log(this);
+// }
+
+// sayName();
