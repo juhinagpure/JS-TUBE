3a2dddde3e899f12279bea403289364218c741ec
JuhiNagpure <juhinagpure143@gmail.com>
2022-07-24 15:19:36 +0530

    Array

---
+++ 03 Intermediate/07 Array.js
@@ -0,0 +1,24 @@
+var countries = ["India", "USA", "Japan", "Russia"];
+
+var states = new Array("Rajasthan", "Delhi", "Mumbai", "Assam")
+
+// console.log(states[1]);
+
+// console.log(states.length);
+states[0]= "punjab";
+// console.log(states);
+
+var user = ["juhi","juhi143",5,8,true];
+console.log(user);
+
+user.pop();
+user.pop();
+// console.log(user);
+user.unshift("NEW VALUE");
+// console.log(user);
+user.shift();
+console.log(user);
+
+console.log(user.indexOf("juhi"));
+
+console.log(Array.from("juhi"));
