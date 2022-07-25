7989cde5fd7af08b60816966b55c03dcb128df0d
JuhiNagpure <juhinagpure143@gmail.com>
2022-07-25 22:20:36 +0530

    filter array & slice & splice

---
+++ 03 Intermediate/09 filter-Array-&-slice-&-splice.js
@@ -0,0 +1,20 @@
+var testArray = [2, 4, 6, 3, 1, 5, 9, 8];
+
+console.log(testArray.fill("h", 2, 5));
+
+const myNumber = [23, 24, 25, 55, 66, 77, 87,98]
+
+const result = myNumber.filter( ( num) => num < 55 );
+
+// console.log(result);
+
+// yes ,same exercise file 
+
+var users = ["Ted", "Tim", "Tom", "Sam", "Sor", "sod"];
+
+// console.log(users.slice(1));
+
+users.splice(1, 3, "HI", "BYE");
+// users.splice()
+console.log(users);
+
