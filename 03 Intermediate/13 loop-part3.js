749129a3f774ec11a8d336b8b92f4089e6f9bbd2
JuhiNagpure <juhinagpure143@gmail.com>
2022-07-29 21:37:31 +0530

    loop-part3

---
+++ 03 Intermediate/13 loop-part3.js
@@ -0,0 +1,19 @@
+const names = ["youtube", "facebook", "Instagram", "Netflix", "Amazon"]
+
+// for (const n of names) {
+//  console.log(n);
+// }
+
+const symbols = {
+   yt: "youtube",
+   ig: "Instagram",
+   fb: "Fcacebook",
+   lco: "LearnCodeOnline.in" 
+}
+
+for(const n in symbols) {
+    console.log(`key is ${n} and value is: ${symbols[n]}`);
+
+}
+
+
