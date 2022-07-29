eff888239ae22f0e7d574649537f73e5a310df49
JuhiNagpure <juhinagpure143@gmail.com>
2022-07-29 20:22:23 +0530

    for Each loop

---
+++ 03 Intermediate/13 loop-part2.js
@@ -0,0 +1,21 @@
+const myStates = [
+    "Rajasthan", 
+    "Delhi", 
+    "Tamil Nadu",
+    1974,
+    "Assam", 
+    "Maharashtra",
+    ];
+
+//     var i = 0;
+
+//     for(;;){
+//         if(i > 3) break;
+//         console.log(i);
+//         i++;
+// }
+    
+
+myStates.forEach( (s) => (console.log(s)) );
+
+myStates.forEach( (s) => console.log(s) );
