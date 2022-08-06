34312539dc28fdc2310594e94b875f1bf93d55a6
JuhiNagpure <juhinagpure143@gmail.com>
2022-08-06 09:50:31 +0530

    strings

---
+++ 06 Advanceish/02 Strings.js
@@ -0,0 +1,10 @@
+var name = `juhi`;
+var fullName = `${name} Nagpure`
+
+
+console.log(fullName);
+console.log(fullName.charAt(0));
+console.log(fullName.endsWith("?"));
+console.log(fullName.includes("juhi"));
+console.log(fullName.toUpperCase());
+console.log(fullName.toLowerCase());
