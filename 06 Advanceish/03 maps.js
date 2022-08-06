ecf52c7b12de632f34f0b8d9bd14e2b063e12625
JuhiNagpure <juhinagpure143@gmail.com>
2022-08-06 10:08:45 +0530

    Initial commit

---
+++ 06 Advanceish/03 maps.js
@@ -0,0 +1,27 @@
+var myMap = new Map();
+
+myMap.set(1, "Uno");
+myMap.set(2, "dos");
+myMap.set(3, "Tres");
+myMap.set(4, "Cuatro");
+
+console.log(myMap);
+
+
+
+// for(let key of myMap.keys()){
+//     console.log(`key is ${key}`);
+// }
+
+// for(let value of myMap.values()){
+//     console.log(`key is ${value}`);
+// }
+
+for(let [key, value] of myMap) {
+    console.log(`key is ${key} and value is ${value}`);
+}
+
+myMap.forEach( (v, k) => console.log(`${v} and key is ${k}`));
+
+myMap.delete(2);
+console.log(myMap);
