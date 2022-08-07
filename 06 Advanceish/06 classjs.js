44c4a20f975afb356c02d26e1eb859d284fe83de
juhinagpure <juhinagpure143@gmail.com>
2022-08-07 18:11:02 +0530

    class object

--- 06 Advanceish/06 classjs.js
+++ 06 Advanceish/06 classjs.js
@@ -15,7 +15,15 @@ enrollCourse(name){
 getCourseList() {
     return this.courseList;
 }
+
 }
 
-module.exports = User;
+export default User;
+
+
+const rock = new User("rock", "rock@rock.com")
+console.log(rock.getInfo());
+rock.enrollCourse("Angular Bootcampm")
+console.log(rock.getCourseList());
+console.log(rock.courseList);
 
--- 06 Advanceish/07 classObjects.js
+++ 06 Advanceish/07 classObjects.js
@@ -1,6 +1,6 @@
-// import User from "./06 classjs";
+import User from "./06 classjs";
 
-const User = require("./06 classjs");
+// const User = require("./06 classjs.js");
 
 
 const juhi = new User("juhi", "juhinagpure143@gmail.com")
@@ -10,6 +10,8 @@ juhi.enrollCourse("React Bootcamp");
 juhi.enrollCourse("Angular Bootcamp");
 
 console.log(juhi.getCourseList());
+
+let courses = juhi.getCourseList()
 
 
 courses.forEach(c => console.log(c));
